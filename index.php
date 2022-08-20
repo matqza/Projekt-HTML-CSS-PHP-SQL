5da36c06de04e6913413d07c035342672975cec7
matqza <the.matqza@gmail.com>
2022-08-20 06:32:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Login Site</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <form action="" class="form">
+            <h1>Login</h1>
+            <input type="email" name="email" class="box" placeholder="Enter Username">
+            <input type="password" name="password" class="box" placeholder="Enter Password">
+            <input type="submit" value="LOGIN" id="submit">
+            <a href="register/register.php">Register Here</a>
+        </form>
+    </body>
+</html>
---
+++ register/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Registration Site</title>
+        <link rel="stylesheet" href="registerstyle.css">
+    </head>
+    <body>
+        <form action="" class="form" method="post">
+            <h1>Registration</h1>
+            <input type="name" name="name" class="box" placeholder="Enter your name">
+            <input type="forname" name="forname" class="box" placeholder="Enter your forname">
+            <input type="username" name="username" class="box" placeholder="Enter your username">
+            <input type="email" name="email" class="box" placeholder="Enter your mail">
+            <input type="password" name="password" class="box" placeholder="Enter Password">
+            <input type="password" name="password" class="box" placeholder="Repeat Password">
+            <input type="submit" value="REGISTER" id="submit">
+
+        </form>
+    </body>
+</html>
