6e6234a10c952b53b3c3717a3b66f526cd27cd69
matqza <the.matqza@gmail.com>
2022-08-21 06:18:30 +0200

    register zrobione warunki

--- register/register.php
+++ register/register.php
@@ -3,14 +3,23 @@
     {
         $pass = $_POST['pass'];
         $pass2 = $_POST['pass2'];
+        $name  = $_POST['name'];      
+        $forname = $_POST['forname'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
 
-        if(empty($pass))
+
+        if(empty($pass) || empty($pass2) || empty($name) || empty($forname) || empty($username) || empty($email))
         {
-            $error_msg['pass']= '<span style="color:#AFA;">Enter the password</span>';
+            $error_msg['error']= '<span style="color:#AFA;">Please complete all fields</span>';
         }
-        if(empty($pass2))
+        else if(strlen($pass) < 5 || strlen($pass2) < 5 || strlen($name) < 5 || strlen($forname) < 5 || strlen($username) < 5 || strlen($email) < 5 )
         {
-            $error_msg['pass2']= '<span style="color:#AFA;">Enter the password</span>';
+            $error_msg['error2']='<span style="color:#AFA;">fields had to be over 5</span>';
+        }
+        else if($pass != $pass2)
+        {
+            $error_msg['pass']= '<span style="color:#AFA;">passwords are different from each other</span>';
         }
     }
 ?>
@@ -37,19 +46,20 @@
             <input type="username" name="username" class="box" placeholder="Enter your username">
             <input type="email" name="email" class="box" placeholder="Enter your mail">
             <input type="password" name="pass" class="box" placeholder="Enter Password">
-            <div id="xxx">
-            <?php  
-            if(isset($error_msg['pass'])){
-                echo $error_msg['pass'];
-            }
-            ?>
-            </div>
             <input type="password" name="pass2" class="box" placeholder="Repeat Password">
-            <div id="xxx">
+            <div id="xxx" name="error">
             <?php  
-            if(isset($error_msg['pass2'])){
-               echo $error_msg['pass2'];
+            if(isset($error_msg['error'])){
+               echo $error_msg['error'];
             }
+            else if(isset($error_msg['error2'])){
+                echo $error_msg['error2'];
+             }
+            
+            else if(isset($error_msg['pass'])){
+                echo $error_msg['pass'];
+             }
+
             ?>
             </div>
 
